fdd9308f3b91b9f26fa591f17dffc2481153798a
yeop <rlawoduq12@gmail.com>
2022-08-22 00:38:02 +0900

    GetController
    get방식 mapping

---
+++ hello/src/main/java/study/springboot/hello/controller/GetController.java
@@ -0,0 +1,61 @@
+package study.springboot.hello.controller;
+
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import study.springboot.hello.dto.MemberDTO;
+
+@RestController
+@RequestMapping("/api/v1/get-api")
+public class GetController {
+
+	@RequestMapping(value="/hello", method=RequestMethod.GET)
+	public String getHello() {
+		return "Hello World";
+	}
+	
+	@GetMapping(value="/name")
+	public String getName() {
+		return "Flature";
+	}
+	
+	@GetMapping(value="/variable1/{variable}")
+	public String getVariable1(@PathVariable String variable) {
+		return variable;
+	}
+	
+	@GetMapping(value="/variable2/{variable}")
+	public String getVariable2(@PathVariable("variable") String var) {
+		return var;
+	}
+	
+	@GetMapping(value="/request1")
+	public String getRequestParam1(
+			@RequestParam String name,
+			@RequestParam String email,
+			@RequestParam String organization) {
+		return name + " " + email + " " + organization;
+	}
+	
+	@GetMapping(value="/request2")
+	public String getRequestParam2(@RequestParam Map<String, String> param) {
+		StringBuilder sb = new StringBuilder();
+		
+		param.entrySet().forEach(map->{
+			sb.append(map.getKey() + " : " + map.getValue() + "\n");
+		});
+		
+		return sb.toString();
+	}
+	
+	@GetMapping(value="/request3")
+	public String getRequestParam3(MemberDTO dto) {
+		return dto.toString();
+	}
+}
